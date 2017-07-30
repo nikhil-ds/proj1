3aa2953c0ac2495c645121192ace8ebef71b6ec4
nikhil-ds <prasadnikhil25@gmail.com>
2017-07-30 10:57:52 +0530

    added fibo module and called it in hello.py and printed series

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 #imoprt the module date.py
+#import fibo.py
 print "Goodmorning to all"
 
 msg = 'goodnight to all'
 print msg
-
+# call the function from fibo module and print the series
 #call the function from date.py & print the date
